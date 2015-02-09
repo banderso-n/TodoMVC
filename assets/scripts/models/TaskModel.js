f4e5db17e3f961e101560eb6d72fe4a1a8e752c9
Brad Anderson <banderso@nerdery.com>
2015-02-09 13:11:46 -0600

    Adds yuidocs to TaskModel properties.

--- assets/scripts/models/TaskModel.js
+++ assets/scripts/models/TaskModel.js
@@ -4,10 +4,31 @@ define(function (require) {
 
     function TaskModel () {
 
+        /**
+         * A human-readable description of the task.
+         *
+         * @property taskModel.description
+         * @type {String}
+         * @default ''
+         */
         this.description = '';
 
+        /**
+         * A unique identifier for the task instance.
+         *
+         * @property taskModel.id
+         * @type {Number}
+         * @default -1
+         */
         this.id = -1;
 
+        /**
+         * Indicates whether or not the task instance is complete.
+         *
+         * @property taskModel.isComplete
+         * @type {Boolean}
+         * @default false
+         */
         this.isComplete = false;
 
     }
